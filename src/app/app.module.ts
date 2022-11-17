8e3db82713368d4bd6caa4ed50a4487ae1ade474
Janosik203 <JanCode203@gmail.com>
2022-11-17 19:51:36 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { InboxComponent } from './inbox/inbox.component';
+import { EmailDetailsComponent } from './email-details/email-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    NavigationComponent,
+    InboxComponent,
+    EmailDetailsComponent
   ],
   imports: [
     BrowserModule
